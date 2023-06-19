b938377360ef46c3acf071c7386eedba60a28a59
Ketel28 <96288839+Ketel28@users.noreply.github.com>
2023-06-19 13:44:09 +0200

    Creating new Spring Boot project

---
+++ src/main/java/com/ubs/m295/M295KeremTelliApplication.java
@@ -0,0 +1,13 @@
+package com.ubs.m295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M295KeremTelliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M295KeremTelliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubs/m295/M295KeremTelliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubs.m295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M295KeremTelliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
